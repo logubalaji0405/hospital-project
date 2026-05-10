26cddbf1cec3873242728942114aca35ee0088c4
012balaji <144882695+012balaji@users.noreply.github.com>
2026-05-10 20:04:08 +0530

    fix otp mail direct send

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -92,20 +92,23 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# ✅ GMAIL SMTP (FREE)
-# EMAIL SMTP SETTINGS
-
+# ✅ BREVO SMTP EMAIL SETTINGS
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 
-DEFAULT_FROM_EMAIL = "Healix Hospital <healix.hms@gmail.com>"
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL",
+    "Healix Hospital <healix.hms@gmail.com>"
+)
+
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 EMAIL_TIMEOUT = 60
-SITE_URL = "https://hospital-project-etq9.onrender.com"
+SITE_URL = os.getenv("SITE_URL", "https://hospital-project-etq9.onrender.com")
