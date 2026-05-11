568c65d5102b3f12d68f07625cd38ca640d6bae0
012balaji <144882695+012balaji@users.noreply.github.com>
2026-05-11 14:06:54 +0530

    fix brevo sender email

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -116,7 +116,10 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 BREVO_API_KEY = os.getenv("BREVO_API_KEY", "")
 
-DEFAULT_FROM_EMAIL = "healix.hms@gmail.com"
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL",
+    "Healix Hospital <healix.hms@gmail.com>"
+)
 
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 EMAIL_TIMEOUT = 3
